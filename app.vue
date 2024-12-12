389357ae7c3a8c59f079486b98d9b64dfe41e747
Muhammad Turmudzi <muhammad.turmudzi@mncgroup.com>
2024-12-13 00:21:32 +0700

    add eslint

--- app.vue
+++ app.vue
@@ -1,13 +1,38 @@
 <script setup lang="ts">
+const colorMode = useColorMode()
 const counter = ref(0)
 </script>
 
 <template>
   <div>
-    <Button @click="counter++">Counter++</Button>
-    <Button variant="destructive" @click="counter--">Counter--</Button>
-    <p class="text-red-600">{{ counter }}</p>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <Button
+      @click="counter++"
+    >
+      Counter++
+    </Button>
+
+    <Button
+      variant="destructive"
+      @click="counter--"
+    >
+      Counter--
+    </Button>
+
+    <p class="text-red-600">
+      {{ counter }}
+    </p>
+
+    <Button @click="colorMode.preference = 'light'">
+      Light
+    </Button>
+    <Button @click="colorMode.preference = 'dark'">
+      Dark
+    </Button>
+    <Button @click="colorMode.preference = 'system'">
+      System
+    </Button>
+
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
   </div>
 </template>
---
+++ eslint.config.mjs
@@ -0,0 +1,53 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+import pluginVue from 'eslint-plugin-vue'
+// import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default withNuxt([
+  // Spread Vue recommended rules directly
+  ...pluginVue.configs['flat/recommended'],
+  // Add Prettier configuration
+  // eslintConfigPrettier,
+  // Custom rules and options
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        },
+        parser: '@typescript-eslint/parser'
+      }
+    },
+    rules: {
+      // Vue-specific rules
+      'vue/no-unused-vars': 'error',
+      'vue/multi-word-component-names': [
+        'off',
+        {
+          ignores: []
+        }
+      ],
+      // General rules
+      'linebreak-style': 0,
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      'no-unused-vars': ['error'],
+      'no-trailing-spaces': ['error', { skipBlankLines: false }],
+      'no-irregular-whitespace': 'error',
+      'no-multiple-empty-lines': 'error',
+      camelcase: 'error',
+      'vue/require-default-prop': 'off',
+    },
+    ignores: [
+      // Files or directories to ignore
+      'node_modules/',
+      'dist/',
+      '.output/',
+      '.nuxt/',
+      'public/'
+    ]
+  }
+])
+
