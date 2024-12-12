63aac72e01cdd72bb2956156d4db7713626cf0b4
Muhammad Turmudzi <muhammad.turmudzi@mncgroup.com>
2024-12-12 22:07:43 +0700

    add shadcn/tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+const counter = ref(0)
+</script>
+
 <template>
   <div>
+    <Button @click="counter++">Counter++</Button>
+    <Button variant="destructive" @click="counter--">Counter--</Button>
+    <p class="text-red-600">{{ counter }}</p>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
